57076c3b988bd5dcbae28a685497e6643bb48c66
lizmanchego <karinamanchego0416@gmail.com>
2022-03-24 08:55:08 -0500

    include Actividad Didactica

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -14,9 +14,26 @@
         .tarjeta.bg-suave.p-2
           .row.justify-content-around.align-items-center
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0.px-lg-2 Selección múltiple con única respuesta / Falso - Verdadero.
+              p.fw-bold.mb-0.px-lg-2 Falso - Verdadero.
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/story.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/AD01-FalsoVerdadero/story.html')" target="_blank")
+                span Realizar
+                i.fas.fa-puzzle-piece
+    
+    .row.mb-4.justify-content-center.align-items-lg-stretch
+      .col-6.col-md-4.col-lg-3.mb-4.mb-md-0.mb-lg-5(data-aos="fade-right")
+        .tarjeta.tarjeta--gris.h-100.d-flex.align-items-center.p-lg-2
+          figure.px-lg-5
+            img.px-lg-2.py-lg-4(src="@/assets/curso/act-didactica.svg", alt="Repaso")
+      .col-12.col-md-8.col-lg-9.mb-lg-5(data-aos="fade-left")
+        h4 Producción de elementos gráficos.
+        p.mb-4 <strong>Objetivo:</strong> Apropiar conceptos para la producción de elementos gráficos mediante un proceso de investigación y creación, para poder elaborar productos en un software especializado.
+        .tarjeta.bg-suave.p-2
+          .row.justify-content-around.align-items-center
+            .col-sm.mb-3.mb-sm-0
+              p.fw-bold.mb-0.px-lg-2 Selección múltiple con única respuesta
+            .col-auto
+              a.boton.boton--b(:href="obtenerLink('/actividades/AD02-SeleccionMultiple/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
 
